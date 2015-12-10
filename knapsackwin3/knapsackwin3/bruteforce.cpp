d408e3b3ada49ca771d9acb4eec26c2d5e5c2c7d
Geoffrey Cline <gdcwd5@mail.mst.edu>
2015-12-09 23:47:49 -0600

    working paralell

--- knapsackwin3/knapsackwin3/bruteforce.cpp
+++ knapsackwin3/knapsackwin3/bruteforce.cpp
@@ -1,56 +1,62 @@
 #include "bruteforce.h"
 #include <cmath>
+#include <windows.h>
+#include <ppl.h>
+#include <mutex>
+#include <atomic>
 
 int bruteforceKS(const KS_List& item, const int maxw)
 {
   
   int n = item.getsize(); //used multiple times so variable for total amount of items
-  
-  int* A = new int[n]; //array of binary values to create all combos
 
-  int currentWeight=0; //used to keep track of weight in currentChoice
-  int currentValue=0;  //same for above except for value
+
   int bestValue=0;     //keeps track of max value obtained by a combo of items
   int bestWeight=0;    //same as above except for weight
 
-#pragma loop(hint_parallel(8))
-  for(int i=1;i<pow(2,n);i++)
-  {
-    currentValue=0;     //resets variables so a new combo can be made
-    currentWeight=0;
+	mutex geoff; 
 
-    for(int x=0;x<item.getsize();x++) //sets all elements to 0
-    {
-      A[x]=0;
-    }
-    
-    //set value of binary in A to i;
-    
-    for (int j = 0; j < n; ++j) 
-    {
-      A[j] = i & (1 << j) ? 1 : 0;
-    }
+	concurrency::parallel_for(int(1), static_cast<int>(pow(2, n)), [&](int i)
+	{
+		int currentV = 0;
+		int currentW = 0;
 
-    for(int k =0; k<n;k++) //creates combo out of items and keeps track of weight and value
-    {
-      if(A[k] == 1)
-      {
-      	currentWeight += item[k] -> getweight();
-      	currentValue += item[k] -> getvalue();
-      }
+		int* A = new int[n];
 
-    }
+		for (int x = 0; x<item.getsize(); x++) //sets all elements to 0
+		{
+			A[x] = 0;
+		}
 
-    if((currentValue > bestValue) && (currentWeight <= maxw)) //replaces max if needed
-    {
-      bestValue = currentValue;
-      bestWeight = currentWeight;
-    }
-     
-    
+		for (int j = 0; j < n; ++j)
+		{
+			A[j] = i & (1 << j) ? 1 : 0;
+		}
 
-  }
-  
+		for (int k = 0; k<n; k++) //creates combo out of items and keeps track of weight and value
+		{
+			if (A[k] == 1)
+			{
+				currentW += item[k]->getweight();
+				currentV += item[k]->getvalue();
+			}
+
+		}
+
+		geoff.lock();
+
+		if ((currentV > bestValue) && (currentW <= maxw)) //replaces max if needed
+		{
+			bestValue = bestValue;
+			bestWeight = currentW;
+		}
+
+		geoff.unlock();
+
+		delete A;
+
+	});
+	   
 
   return bestValue; //returns max set
 
